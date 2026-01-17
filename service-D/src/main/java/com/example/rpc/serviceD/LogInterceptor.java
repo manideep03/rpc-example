fc710317044beec3afd04abc114a923584be4772
Manideep Gujjari <manideep.gujjari@gmail.com>
2026-01-17 20:59:29 +0530

    Few things: 1) Request interceptor 2) Timeout with rpc calls

---
+++ service-D/src/main/java/com/example/rpc/serviceD/LogInterceptor.java
@@ -0,0 +1,19 @@
+package com.example.rpc.serviceD;
+
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
+
+@Slf4j
+@GrpcGlobalServerInterceptor
+public class LogInterceptor implements ServerInterceptor {
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+            ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        log.info("Service called: {}",  call.getMethodDescriptor().getServiceName());
+        return next.startCall(call, headers);
+    }
+}
